bd3228d482cf0a9960941c0e673b3334d912ff4a
Rômulo Henri <romulohenri000@gmail.com>
2023-09-24 09:39:25 -0300

    improve: QuizPage

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import manThinking from '../assets/man-thinking-bro.svg'
 
 const QuizPage = () => {
   return (
     <main>
-      <section className='welcome'>
+      <section className='quizzes'>
         <div className='container'>
-          <div>
-            
-          </div>
           <div>
             <h2>Quizzes:</h2>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem exercitationem reprehenderit ipsa quasi dignissimos excepturi fugit. Eaque, beatae harum aspernatur quisquam illo laboriosam repellat eligendi ea excepturi expedita omnis! Sed!</p>
+          </div>
+          <div>
+            <img src={manThinking} alt="" />
           </div>
         </div>
       </section>
